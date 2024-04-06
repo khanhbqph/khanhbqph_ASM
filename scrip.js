385372c72507cd30657cffcd8904ce09de9f4c74
khanhbqph <khanhbqph21610@fpt.edu.vn>
2024-04-07 01:29:44 +0700

    Asm2

--- scrip.js
+++ scrip.js
@@ -19,3 +19,12 @@ function myFunction() {
     x.className = "container";
   }
 }
+
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "container") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "container";
+//   }
+// }
